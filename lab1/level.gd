634cceb6013e2235d5e3be98d87fb0342366af0f
Charles L <charlesliu618@gmail.com>
2025-09-26 09:15:31 -0600

    Initial commit

---
+++ lab1/level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+#const playerScene = preload("res://player.tscn")
+#
+#var player = playerScene.instantiate()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
