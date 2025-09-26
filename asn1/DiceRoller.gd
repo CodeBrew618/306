5b5da2f3640f8af2fb81542b07a50e7e2853cafa
Charles L <charlesliu618@gmail.com>
2025-09-26 10:08:39 -0600

    add documentations for the methods, described the purpose, usage and return values.

--- asn1/DiceRoller.gd
+++ asn1/DiceRoller.gd
@@ -1,28 +1,42 @@
 extends RefCounted
 class_name DiceRoller
 
-##
+##This class is a Dice rolling class.
+##To use this class, you need to specify the number of dice, and the number of each dice.
+## Class will have getter and setter and a roll function.
 
+
+# Number of dice to roll
 var n_Dice: int
+# Number of sides on each dice
 var n_Side: int
 
-
+## Constructor: Initializes a new DiceRoller instance.
+## @param n_d The number of dice to be rolled.
+## @param n_s The number of sides on each die .
 func _init(n_d:int, n_s:int) -> void:
 	n_Dice = n_d
 	n_Side = n_s
-	
+
+##Sets the number of total dices 	
 func set_n_dice(n:int) -> void:
 	n_Dice = n
 	
+##Sets the number of dice sides with int.	
 func set_n_side(n:int) -> void:
 	n_Side = n
 	
+##Return the number of dices.
 func get_n_dice(n:int) -> int:
 	return n_Dice
-	
+
+##Return the number of dice's side
 func get_n_side(n:int) -> int:
 	return n_Side
 	
+	
+##Simulates a roll and returns the results in an array.
+##Return wiht an array that contains the result of each dices.	
 func get_roll() -> Array[int]:
 	pass
 
